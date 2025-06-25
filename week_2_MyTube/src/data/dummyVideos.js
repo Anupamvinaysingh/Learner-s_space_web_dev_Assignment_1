5ded401742f2b8e0ea58936145ad32bac30eaedf
Anupamvinaysingh <anupambihat@gmail.com>
2025-06-25 23:12:33 +0530

    Create dummyVideos.js

---
+++ week_2_MyTube/src/data/dummyVideos.js
@@ -0,0 +1,18 @@
+export const videos = [
+  {
+    id: 1,
+    title: "React Hooks Explained",
+    channel: "CodeWithYou",
+    views: "1.2M",
+    time: "2 days ago",
+    thumbnail: "https://via.placeholder.com/300x170.jpg",  //The provided placeholder website is not opening in my device, I don't know why, still I added it as mentioned in the sample code.
+  },
+  {
+    id: 2,
+    title: "Flexbox in 10 minutes",
+    channel: "CSSNinjas",
+    views: "860K",
+    time: "1 week ago",
+    thumbnail: "https://via.placeholder.com/300x170",
+  },
+];
