fea5f080dd55f9362eac0ad8e08d61c20aa00ed9
Anupamvinaysingh <anupambihat@gmail.com>
2025-07-09 19:46:58 +0530

    Delete Final_Assignment/ytclone/frontend directory

--- Final_Assignment/ytclone/frontend/src/components/Dashboard.js
+++
@@ -1,4 +0,0 @@
-
-import React from 'react';
-const Login = () => <h2>Dashboar</h2>;
-export default Login;
--- Final_Assignment/ytclone/frontend/src/components/Sidebar.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-const Login = () => <h2>Sidebar</h2>;
-export default Login;
